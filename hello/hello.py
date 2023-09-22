dba5be2529aab88b3b56ce05c77ce47fcd1263f2
octopuslearn <120025758+octopuslearn@users.noreply.github.com>
2023-09-22 23:06:18 +0800

    将首字母改成大写

--- hello/hello.py
+++ hello/hello.py
@@ -1,5 +1,6 @@
 message = "oc_learn###"
 print(message)
 
-message = "welcome~~~"
-print(message)
+message = "welcome"
+print(message)  #welcom
+print(message.title())  #title()方法，将开头的字母从小写改成大写--->Welcom
