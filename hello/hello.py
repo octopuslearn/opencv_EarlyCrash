d4867488b6ba7eb5e250259537ce794e5557e2e2
octopuslearn <120025758+octopuslearn@users.noreply.github.com>
2023-09-23 09:41:19 +0800

    insert方法插入 #在当前位置插入，其余元素会右移
    ebood.insert(0, 'my')   #insert方法在任意位置插入元素
    ebood.insert(4,"it is a state of ")
    ebood.insert(6,'mind')  #['my', 'youth', 'is', 'not', 'it is a state of ', 'a time of life.', 'mind']
                            #在当前位置插入，其余元素会右移

--- hello/hello.py
+++ hello/hello.py
@@ -5,4 +5,11 @@ ebood.append('is')
 ebood.append("not")
 ebood.append("a time of life.")
 
+print(ebood)   
+
+
+ebood.insert(0, 'my')   #insert方法在任意位置插入元素
+ebood.insert(4,"it is a state of ")
+ebood.insert(6,'mind')  #['my', 'youth', 'is', 'not', 'it is a state of ', 'a time of life.', 'mind']
+                        #在当前位置插入，其余元素会右移
 print(ebood)    
